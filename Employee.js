a4045c4cdd70be4f1f0509d2608ae4bc266bc660
vatsal <vatsaljain020503@gmail.com>
2025-03-20 00:08:03 +0530

    [Vatsal]Add:adding code of UC9 into branch

--- Employee.js
+++ Employee.js
@@ -170,7 +170,7 @@ console.log("UC 7G – Number of Days Emp Worked: " + empDailyWageArr.reduce(tot
  totalWorkingDays = 0;
  empDailyWageArr = new Array();
 let empDailyWageMap = new Map();
-
+let empDailyHrsMap = new Map();
 function calcDailyWage(empHrs) {
     return empHrs * WAGE_PER_HOUR;
 }
@@ -191,3 +191,27 @@ function totalWages(totalWage, dailyWage) {
 }
 console.log("UC7A – Emp Wage Map totalWages: " +
             Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+// UC 9 Arrow Functions
+
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0)
+                                 .reduce(findTotal, 0);
+console.log("UC9A – Emp Wage with Arrow: " + " Total Hours: " + 
+            totalHours + " Total Wages: " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days: " + fullWorkingDays);
+console.log("Part Working Days: " + partWorkingDays);
+console.log("Non Working Days: " + nonWorkingDays);
